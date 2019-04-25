0ba29cb0069805a117ba0b814eab6676b25e3ee5
Tony Baker <this.tonybaker@gmail.com>
2019-04-25 19:36:46 +0000

    Automatically backed up by Learn

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,14 @@
 require_relative '../config/environment'
+<<<<<<< HEAD
 Hashie
 Sinatra
 Octokit
 Pry
 AwesomePrint
+=======
+# Hashie
+# Sinatra
+# Octokit
+# Pry
+# AwesomePrint
+>>>>>>> a9f5798636204b501fef207a8dbf522e41c3c86d
